85b871604260d219b731364a22860099954143a1
Frej Raahede Nielsen <frejraahede@gmail.com>
2020-08-04 16:24:42 +0200

    Use normalize.css

--- src/vue/index.js
+++ src/vue/index.js
@@ -1,10 +1,13 @@
 'use strict';
 
 import UiTest from './components/UiTest';
+import UiCard from './components/UiCard';
+import 'normalize.css/normalize.css';
 import '../styl/main.styl';
 
 export default {
   install(Vue, options) {
     Vue.use(UiTest);
+    Vue.use(UiCard);
   }
 };

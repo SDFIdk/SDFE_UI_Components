c1b89f211492c83f552e82dfa040225f1d177171
Frej Raahede Nielsen <frejraahede@gmail.com>
2020-09-02 13:07:11 +0200

    Cleanup component

--- src/vue/components/UiTest/UiTest.vue
+++ src/vue/components/UiTest/UiTest.vue
@@ -1,25 +1,9 @@
 <template>
-  <div>
-    Test Component
-    <!-- <img svg-inline class="icon" src="../../../assets/icons/icon-user.svg" alt="example" /> -->
-    <icon-user />
-  </div>
+  <div>Test Component</div>
 </template>
 
 <script>
 export default {
-  name: 'UiTest',
-  components: {
-    // 'icon-user': () => import('../../../assets/icons/icon-user.svg')
-    // IconUser
-  },
-  props: {
-    name: String
-  },
-  // mounted() {
-  //   this.$nextTick(() => {
-  //     this.ready = true
-  //   })
-  // }
+  name: 'UiTest'
 }
 </script>

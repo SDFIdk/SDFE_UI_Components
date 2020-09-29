bdd72b813c13881c84459d2971bb5067b9cc9869
Frej Raahede Nielsen <frejraahede@gmail.com>
2020-09-29 13:57:09 +0200

    Add form select

---
+++ src/vue/components/UiFormSelect/UiFormSelect.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="ui-form-control">
+    <label
+      v-if="label"
+      class="ui-form-label"
+      :for="id"
+    >{{ label }}</label>
+    <select
+      :id="id"
+      v-model="model"
+      class="ui-form-input"
+      @change="$emit('change', $event.target.value)"
+    >
+      <option
+        v-for="(option, index) in options"
+        :key="`${ id }_option_${ index }`"
+        :value="option.value"
+      >
+        {{ option.label }}
+      </option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UiFormSelect',
+  props: {
+    label: {
+      type: String,
+      default: null
+    },
+    value: {
+      type: String,
+      default: ''
+    },
+    options: {
+      type: Array,
+      default: () => []
+    }
+  },
+  data () {
+    return {
+      id: `select_${this._uid}`,
+      model: this.value
+    }
+  },
+  watch: {
+    value (newVal, oldVal) {
+      this.model = newVal
+    }
+  }
+}
+
+</script>
---
+++ src/vue/components/UiFormSelect/index.js
@@ -0,0 +1,9 @@
+'use strict'
+
+import UiFormSelect from './UiFormSelect.vue'
+
+export default {
+  install (Vue, options) {
+    Vue.component(UiFormSelect.name, UiFormSelect)
+  }
+}
